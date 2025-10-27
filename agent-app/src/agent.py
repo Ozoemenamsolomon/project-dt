420feec7ef3554c2f37ff66a6c0c47b5a56fff36
solozo <ozoemenamsolomon@yahoo.com>
2025-10-27 23:59:05 +0100

    comment mcp_client for now

--- agent-app/src/agent.py
+++ agent-app/src/agent.py
@@ -1,14 +1,14 @@
 from langchain.agents import create_agent
 from middleware.error import handle_tool_errors
 from tools.sample import send_email
-from mcp.atlassian import mcp_client
+# from mcp.atlassian import mcp_client
 
 
-mcp_tools = mcp_client.get_tools()
+# mcp_tools = mcp_client.get_tools()
 # TODO: the mcp_tool types is currently bugging
 """ , *mcp_tools """
 agent = create_agent(
-    "openai:gpt-4o",
+    "openai:gpt-4o-mini",
     tools=[send_email],
     middleware=[handle_tool_errors],
     system_prompt="You are an email assistant. Always use the send_email tool.",
